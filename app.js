bf030fb3cdd425a577a211d5c4eafa514669cdb9
kainat meo <kainatmeo21@gmail.com>
2024-05-11 21:59:19 +0500

    this is my assignment

---
+++ app.js
@@ -0,0 +1,39 @@
+
+// Task 1
+var username;
+// Task 2
+ var myName="Your Full name";
+// Task 3
+ var massage;
+ massage=" Assalam o alikum";
+ alert(massage);
+// Task 4
+ var studentName="Kainat";
+ var studentAge="20";
+ var studentGender="female";
+ var studentTrade="Web and App development";
+
+ alert("studentName=" + studentName);
+ alert("studentAge =" + studentAge);
+ alert("studentGender =" + studentGender);
+ alert("studentTrade =" + studentTrade);
+ 
+//  Task5
+var alertMassagr="This is Alert massage";
+alert(alertMassagr);
+ 
+// Task 6
+var email="example@gamil.com";
+alert("My email Address"  +  email);
+
+// Task 7
+var book="A smart way to lear javaScript";
+alert("I learn this Book"  + book );
+document.write("I learn this Book  "  + book  );
+  
+// Task 8
+var messageToShow = "This is a sample message to show in alert and browser.";
+alert(messageToShow);
+document.write("<br>" + messageToShow);
+
+ 
